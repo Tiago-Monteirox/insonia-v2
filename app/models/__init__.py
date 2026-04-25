7db02367c02e650f358de50839ffa7efae562392
Tiago-monteirox <tiagomonteirodev@outlook.com>
2026-04-25 12:20:33 -0300

    add User to models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,9 +3,10 @@ from app.models.brand import Brand
 from app.models.product import Product, ProductImage
 from app.models.variation import Variation, VariationName, VariationValue
 from app.models.sale import Sale, SaleItem
+from app.models.user import User
 
 __all__ = [
     "Category", "Brand", "Product", "ProductImage",
     "VariationName", "VariationValue", "Variation",
-    "Sale", "SaleItem",
+    "Sale", "SaleItem", "User",
 ]
